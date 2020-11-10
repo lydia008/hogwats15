682fe4624d920ad42e943cdde3f2193167bb23d0
lydia008 <258903198@qq.com>
2020-11-10 16:23:55 +0800

    appium 企业微信实战1  外出打卡

--- app/test_wework.py
+++ app/test_wework.py
@@ -2,8 +2,10 @@
 # -*- coding: utf-8 -*-
 # Date ：2020/11/4 0:38
 # Author：Lydia
-
+from time import sleep
 from appium import webdriver
+from appium.webdriver.common.mobileby import MobileBy
+from selenium.webdriver.support.wait import WebDriverWait
 
 
 class TestWework:
@@ -15,20 +17,35 @@ class TestWework:
             "deviceName": "wework",
             "appPackage": "com.tencent.wework",
             "appActivity": ".launch.WwMainActivity",
-            "noReset": "True"
+            "noReset": "True",
+            # "settings[waitForIdleTimeout]": 0
         }
 
         # 本机ip:server端口; 建立客户端和服务端连接
         self.driver = webdriver.Remote("http://127.0.0.1:4723/wd/hub", caps)
+        # 隐式等待
+        self.driver.implicitly_wait(5)
 
     def teardown(self):
         self.driver.quit()
 
-    def test_wework(self):
-        el1 = self.driver.find_element_by_xpath(
-            "/hierarchy/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.LinearLayout/android.view.ViewGroup/android.widget.RelativeLayout[2]/android.widget.RelativeLayout/android.widget.TextView")
-        el1.click()
-        el2 = self.driver.find_element_by_xpath(
-            "/hierarchy/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.FrameLayout[2]/android.widget.RelativeLayout/android.widget.ListView/android.widget.RelativeLayout[3]/android.widget.RelativeLayout/android.widget.LinearLayout/android.widget.LinearLayout/android.view.ViewGroup/android.widget.TextView")
-        el2.click()
-
+    # 外出打卡
+    def test_case01(self):
+        # 点击工作台
+        self.driver.find_element(MobileBy.XPATH, "//*[@text='工作台']").click()
+        # 滑动找到[打卡]
+        self.driver.find_element(MobileBy.ANDROID_UIAUTOMATOR,
+                                 'new UiScrollable(new UiSelector()\
+                                 .scrollable(true).instance(0))\
+                                 .scrollIntoView(new UiSelector()\
+                                 .text("打卡").instance(0));').click()
+        # setting
+        self.driver.update_settings({"waitForIdleTimeout": 0})
+        # 点击外出打卡
+        self.driver.find_element(MobileBy.XPATH, "//*[@text='外出打卡']").click()
+        # 点击打卡
+        self.driver.find_element(MobileBy.XPATH, "//*[contains(@text,'次外出')]").click()
+        # sleep(3)
+        # assert "外出打卡成功" in self.driver.page_source
+        # 显示等待
+        WebDriverWait(self.driver, 10).until(lambda x: ("外出打卡成功" in x.page_source))
