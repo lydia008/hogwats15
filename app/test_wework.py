8e93008810f4c742523ed7b1bb22ea5e91d81849
lydia008 <258903198@qq.com>
2020-12-01 23:45:47 +0800

    appium 企业微信实战1  手动添加人员

--- app/test_wework.py
+++ app/test_wework.py
@@ -10,14 +10,19 @@ from selenium.webdriver.support.wait import WebDriverWait
 
 class TestWework:
 
-    def setup(self):
+    def setup_class(self):
         # 定义字典
         caps = {
             "platformName": "Android",
             "deviceName": "wework",
             "appPackage": "com.tencent.wework",
             "appActivity": ".launch.WwMainActivity",
+            # noReset 保留缓存， 比如登录状态
             "noReset": "True",
+            # 不停止应用，直接运行测试用例
+            "dontStopAppOnReset": "true",
+            "skipDeviceInitialization": "true",
+            "skipServerInstallation": "true"
             # "settings[waitForIdleTimeout]": 0
         }
 
@@ -27,7 +32,11 @@ class TestWework:
         self.driver.implicitly_wait(5)
 
     def teardown(self):
-        self.driver.quit()
+        self.driver.back()
+
+    def test_a(self):
+        self.driver.find_element(MobileBy.XPATH, "//*[@text='通讯录']").click()
+        self.driver.find_element(MobileBy.XPATH, "//*[@text='添加成员']").click()
 
     # 外出打卡
     def test_case01(self):
@@ -49,3 +58,6 @@ class TestWework:
         # assert "外出打卡成功" in self.driver.page_source
         # 显示等待
         WebDriverWait(self.driver, 10).until(lambda x: ("外出打卡成功" in x.page_source))
+
+    def teardown_class(self):
+        self.driver.quit()
